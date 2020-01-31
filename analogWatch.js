8646d0043360986e17f10ae7007f66e12aecc3ae
Nik Martin <nik.martin@gmail.com>
2020-01-31 13:56:14 -0600

    add day & date

--- analogWatch.js
+++ analogWatch.js
@@ -1,11 +1,16 @@
+const version = '0.0.2';
+
 const p = Math.PI / 2;
 const PRad = Math.PI / 180;
 const hourDegrees = 15;
-let intervalRefSec = null;
+const faceWidth = 95; // watch face is 95 px wide (radius)
+let timerInterval = null;
 let currentDate = new Date();
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+// const g = global.Graphics;
+// const Bangle = global.Bangle; :smile:
 
-
-function seconds(angle, r) {
+const seconds = (angle, r) => {
   const a = angle * PRad;
   const x = 120 + Math.sin(a) * r;
   const y = 120 - Math.cos(a) * r;
@@ -14,9 +19,9 @@ function seconds(angle, r) {
   } else {
     g.fillEllipse(x - 1, y - 1, x + 1, y + 1);
   }
-}
+};
 
-function hand(angle, r1, r2) {
+const hand = (angle, r1, r2) => {
   const a = angle * PRad;
   const r3 = 3;
   g.fillPoly([
@@ -29,9 +34,9 @@ function hand(angle, r1, r2) {
     120 + Math.sin(a - p) * r3,
     120 - Math.cos(a - p) * r3
   ]);
-}
+};
 
-function drawAll() {
+const drawAll = () => {
   g.clear();
   currentDate = new Date();
   // draw hands first
@@ -39,71 +44,64 @@ function drawAll() {
   // draw seconds
   g.setColor(0, 0, 0.6);
   for (let i = 0; i < 60; i++) {
-    seconds((360 * i) / 60, 95);
+    seconds((360 * i) / 60, faceWidth);
   }
   onSecond();
-}
+};
 
-function resetSeconds() {
+const resetSeconds = () => {
   g.setColor(0, 0, 0.6);
   for (let i = 0; i < 60; i++) {
-    seconds((360 * i) / 60, 95);
+    seconds((360 * i) / 60, faceWidth);
   }
-}
+};
 
-function onSecond() {
+const onSecond = () => {
   g.setColor(0.3, 0.3, 1);
-  seconds((360 * currentDate.getSeconds()) / 60, 95);
+  seconds((360 * currentDate.getSeconds()) / 60, faceWidth);
   if (currentDate.getSeconds() === 59) {
     resetSeconds();
     onMinute();
   }
   g.setColor(1, 0.7, 0.2);
   currentDate = new Date();
-  seconds((360 * currentDate.getSeconds()) / 60, 95);
+  seconds((360 * currentDate.getSeconds()) / 60, faceWidth);
   g.setColor(1, 1, 1);
-}
+};
 
-function onMinute() {
+const onMinute = () => {
   g.setColor(0, 0, 0);
   // Hour hand
-  hand(
-    (360 * (currentDate.getHours() + currentDate.getMinutes() / 60)) / 12,
-    -8,
-    55
-  );
-  //Minute Hand
-  hand((360 * currentDate.getMinutes()) / 60, -8, 88);
+  hand((360 * (currentDate.getHours() + currentDate.getMinutes() / 60)) / 12, -8, faceWidth * 0.60);
+  // Minute Hand
+  hand((360 * currentDate.getMinutes()) / 60, -8, faceWidth * 0.90);
   currentDate = new Date();
   g.setColor(1, 0.7, 0.7);
   // Hour Hand
-  hand(
-    (360 * (currentDate.getHours() + currentDate.getMinutes() / 60)) / 12,
-    -8,
-    55
-  );
+  hand((360 * (currentDate.getHours() + currentDate.getMinutes() / 60)) / 12, -8, faceWidth * 0.60);
   g.setColor(1, 1, 0.8);
   // Minute Hand
-  hand((360 * currentDate.getMinutes()) / 60, -8, 88);
+  hand((360 * currentDate.getMinutes()) / 60, -8, faceWidth * 0.90);
   if (currentDate.getHours() >= 0 && currentDate.getMinutes() === 0) {
     Bangle.buzz();
   }
-}
+  g.setColor(1, 1, 1);
+  g.drawString(`${days[currentDate.getDay()]}-${currentDate.getDate().toString().padStart(2, '0')}`, g.getWidth() / 2 + 30, g.getHeight() / 2 + 40);
+};
 
-function clearTimers() {
-  if (intervalRefSec) {
-    clearInterval(intervalRefSec);
+const clearTimers = () => {
+  if (timerInterval) {
+    clearInterval(timerInterval);
   }
-}
+};
 
-function startTimers() {
+const startTimers = () => {
   currentDate = new Date();
-  currentDate = new Date();
-  intervalRefSec = setInterval(onSecond, 1000);
+  timerInterval = setInterval(onSecond, 1000);
   drawAll();
-}
+};
 
-Bangle.on("lcdPower", function (on) {
+Bangle.on('lcdPower', (on) => {
   if (on) {
     g.clear();
     Bangle.drawWidgets();
@@ -113,10 +111,16 @@ Bangle.on("lcdPower", function (on) {
   }
 });
 
+Bangle.on('faceUp', (up) => {
+  if (up && !Bangle.isLCDOn()) {
+    Bangle.setLCDPower(true);
+  }
+});
+
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
-drawAll();
+console.log('😎 Nik\'s watch version ', version);
 startTimers();
 // Show launcher when middle button pressed
 setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
